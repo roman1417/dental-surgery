e4f16f432998c2a0ee6d14770fb5f4cea3f4eb87
roman1417 <roman1417@o2.pl>
2020-06-07 23:06:31 +0200

    Updates

---
+++ precache-manifest.7ce0a74b53de7a5d606f2b52558809e1.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09055306d62cd8029dd58c6b246942fd",
+    "url": "/dental-surgery/index.html"
+  },
+  {
+    "revision": "eff2053ca3c48c9e2e6b",
+    "url": "/dental-surgery/static/css/2.2a6cf6b3.chunk.css"
+  },
+  {
+    "revision": "eff2053ca3c48c9e2e6b",
+    "url": "/dental-surgery/static/js/2.f4214272.chunk.js"
+  },
+  {
+    "revision": "51ccd56507b155fc340d16b580bc1a5d",
+    "url": "/dental-surgery/static/js/2.f4214272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65a67510fc97293be98",
+    "url": "/dental-surgery/static/js/main.b3f4617f.chunk.js"
+  },
+  {
+    "revision": "1386cde4a72dfe1cc78f",
+    "url": "/dental-surgery/static/js/runtime-main.daa45f16.js"
+  },
+  {
+    "revision": "7c7dfbb087e68c6c7f584d1382ac5398",
+    "url": "/dental-surgery/static/media/article1_1280.7c7dfbb0.jpg"
+  },
+  {
+    "revision": "f28f8994f0ac3062488a49b91db7ab9a",
+    "url": "/dental-surgery/static/media/article1_1920.f28f8994.jpg"
+  },
+  {
+    "revision": "a43094219c34657ce9309a05eee05abb",
+    "url": "/dental-surgery/static/media/article1_640.a4309421.jpg"
+  },
+  {
+    "revision": "fa1651dec93bb0f173354cdf53957025",
+    "url": "/dental-surgery/static/media/article2_1280.fa1651de.jpg"
+  },
+  {
+    "revision": "7eba142c49d98a845ec667d38cbb37c5",
+    "url": "/dental-surgery/static/media/article2_1920.7eba142c.jpg"
+  },
+  {
+    "revision": "4222f24b6a50d1cd973678d3a380cd4e",
+    "url": "/dental-surgery/static/media/article2_640.4222f24b.jpg"
+  },
+  {
+    "revision": "f4a2817d18464034d764f37a15a9ee60",
+    "url": "/dental-surgery/static/media/article3_1280.f4a2817d.jpg"
+  },
+  {
+    "revision": "055cbd1887c3866b2a0cfb0a23be8142",
+    "url": "/dental-surgery/static/media/article3_1920.055cbd18.jpg"
+  },
+  {
+    "revision": "1dfcb52396abbaea9340e8a824617d0e",
+    "url": "/dental-surgery/static/media/article3_640.1dfcb523.jpg"
+  },
+  {
+    "revision": "d2ca9ecc7479ecf75de7b328b696e564",
+    "url": "/dental-surgery/static/media/crew1_640.d2ca9ecc.jpg"
+  },
+  {
+    "revision": "67f9f174f3d8bcc59ab5e3da198aa809",
+    "url": "/dental-surgery/static/media/crew2_640.67f9f174.jpg"
+  },
+  {
+    "revision": "d0dd3594981a68ce539b44dc538f658f",
+    "url": "/dental-surgery/static/media/crew3_640.d0dd3594.jpg"
+  },
+  {
+    "revision": "14cb65f834eee9ab490e00f2a4d32645",
+    "url": "/dental-surgery/static/media/crew4_640.14cb65f8.jpg"
+  },
+  {
+    "revision": "da8f4d84b3674d2fcd5502464b30e299",
+    "url": "/dental-surgery/static/media/header1_1280.da8f4d84.jpg"
+  },
+  {
+    "revision": "8873fad25ad616cb8deb9c919bdddcb7",
+    "url": "/dental-surgery/static/media/header1_1920.8873fad2.jpg"
+  },
+  {
+    "revision": "dc0943ce4a945ce7bfa407f128be5471",
+    "url": "/dental-surgery/static/media/header1_640.dc0943ce.jpg"
+  },
+  {
+    "revision": "4945ea4da9eb2739c84cd2da2025b050",
+    "url": "/dental-surgery/static/media/header2_1280.4945ea4d.jpg"
+  },
+  {
+    "revision": "b0b9a6555714fc4e45b372fb9da08597",
+    "url": "/dental-surgery/static/media/header2_1920.b0b9a655.jpg"
+  },
+  {
+    "revision": "0cb070c8a270f05841b4db212a5cb41a",
+    "url": "/dental-surgery/static/media/header2_640.0cb070c8.jpg"
+  },
+  {
+    "revision": "35327e6a2cf2168fbac43f44fc628a0d",
+    "url": "/dental-surgery/static/media/header3_1280.35327e6a.jpg"
+  },
+  {
+    "revision": "bc3051393b1527164aa5af10d2f28af8",
+    "url": "/dental-surgery/static/media/header3_1920.bc305139.jpg"
+  },
+  {
+    "revision": "47cc3afff3b67d2196de4d23af5b9dca",
+    "url": "/dental-surgery/static/media/header3_640.47cc3aff.jpg"
+  },
+  {
+    "revision": "b85563b58752a14637fe83b8d4108537",
+    "url": "/dental-surgery/static/media/home_1280.b85563b5.jpg"
+  },
+  {
+    "revision": "3e883ac86328c4fbfd80912b040747a4",
+    "url": "/dental-surgery/static/media/home_1920.3e883ac8.jpg"
+  },
+  {
+    "revision": "7e75b8bf51defea549197d4cd94b1808",
+    "url": "/dental-surgery/static/media/home_640.7e75b8bf.jpg"
+  },
+  {
+    "revision": "d556b0f1f60b32a29c9f9cbe3aa736cf",
+    "url": "/dental-surgery/static/media/img1_1280.d556b0f1.jpg"
+  },
+  {
+    "revision": "0720a1c31c4f86e6c8e30724fcb6f4af",
+    "url": "/dental-surgery/static/media/img2_1280.0720a1c3.jpg"
+  },
+  {
+    "revision": "ae73e5c41114792796f67b0d1580718f",
+    "url": "/dental-surgery/static/media/img3_1280.ae73e5c4.jpg"
+  },
+  {
+    "revision": "09b343ca7d5e308a35076d0dd8ed8e2d",
+    "url": "/dental-surgery/static/media/img4_1280.09b343ca.jpg"
+  },
+  {
+    "revision": "aa72407f25743b837418a8578ef259af",
+    "url": "/dental-surgery/static/media/img5_1280.aa72407f.jpg"
+  },
+  {
+    "revision": "a31cdc31f3cf682fc76b84ad44035c6c",
+    "url": "/dental-surgery/static/media/thumb1_640.a31cdc31.jpg"
+  },
+  {
+    "revision": "a4659cebeae53f5edc7757947a01d1b6",
+    "url": "/dental-surgery/static/media/thumb2_640.a4659ceb.jpg"
+  },
+  {
+    "revision": "c0cb0a30cc127536d147271fe552bfe2",
+    "url": "/dental-surgery/static/media/thumb3_640.c0cb0a30.jpg"
+  },
+  {
+    "revision": "5e998628d77ba0bebdc2738bd4f10e05",
+    "url": "/dental-surgery/static/media/thumb4_640.5e998628.jpg"
+  },
+  {
+    "revision": "6cc6e6c214997ef685d4bbc764bc07e6",
+    "url": "/dental-surgery/static/media/thumb5_640.6cc6e6c2.jpg"
+  }
+]);
